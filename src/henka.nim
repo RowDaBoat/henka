777cfd1504fed7761ed55a612770ca3c64488275
sOkam <7308253+heysokam@users.noreply.github.com>
2026-04-30 17:14:00 +0200

    rew: Move entry point skeleton out of butcher

---
+++ src/henka.nim
@@ -0,0 +1,22 @@
+
+type CliConfig = object
+  help      {.help: "Show this help message", shortOption: 'h', mode: option.}           : bool
+  clangargs {.help: "Forward arguments to clang".}                                       : string
+  astout    {.help: "Output the generated JSON AST to this path".}                       : string
+  nimout    {.help: "Output the generated Nim bindings to this path (default: stdout)".} : string
+  jsonast   {.help: "Use an existing JSON AST instead of invoking clang".}               : string
+
+when isMainModule:
+  var cli = initCliquet(CliConfig())
+  let headers = cli.parseOptions(commandLineParams())
+  let config = cli.config()
+
+  let usage = "Usage: henka [options] <header.h> [header.h ...]"
+  let help = usage & "\n" & cli.generateHelp()
+  if config.help:
+    echo help
+    quit(0)
+
+  # TODO: Port the CLI application to the new API
+  echo "Hello Henka: TODO"
+
