af1eee1deddfb303a68dbd8d89331a17856b5789
Row <reardenr@gmail.com>
2026-05-02 20:35:42 -0300

    Removed bindings.nim

--- tests/c/variables/bindings.nim
+++
@@ -1,9 +0,0 @@
-var
-  uninit_int* :cint
-  uninit_float* :cfloat
-  uninit_char* :cchar
-  init_int* :cint= 10
-  init_float* :cfloat= 3.140000104904175
-  init_char* :cchar= 'x'
-const const_int* :cint= 42
-var extern_int* :cint
